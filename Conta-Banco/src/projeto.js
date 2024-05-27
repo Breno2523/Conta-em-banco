2834418cacdc496a9d376d80640d607b6e4d3ae1
Breno <dominguesbreno40@gmail.com>
2024-05-27 10:24:12 -0300

    conta banco

--- Conta-Banco/src/projeto.js
+++ Conta-Banco/src/projeto.js
@@ -4,3 +4,4 @@
 /*Permita que os dados sejam inseridos via terminal sendo que o usuário receberá a mensagem de qual informação será solicitada, exemplo:
 * Programa: "Por favor, digite o número da Agência !"
 Usuário: 1021 (depois ENTER para o próximo campo)*/
+// Conhecer e importar a classe Scanner
